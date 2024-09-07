3e3462e1353e0892dae1fbda30a9d73857062bad
TruongHieu <TruongTriHieu@gmail.com>
2024-09-07 17:36:53 +0700

    update code v1

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -6,17 +6,14 @@ import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        # Lấy chuỗi kết nối từ biến môi trường
         connection_string = os.getenv("CosmosDBConnectionString")
         client = pymongo.MongoClient(connection_string)
-        database = client['test']  # Thay đổi tên cơ sở dữ liệu nếu cần
+        database = client['test']  
         collection = database['advertisements']
 
-        # Truy vấn tất cả tài liệu trong collection
         result = collection.find({})
         result = dumps(result)
 
         return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
     except Exception as e:
-        # Ghi lỗi để dễ dàng chẩn đoán vấn đề
         return func.HttpResponse(f"Could not connect to MongoDB: {str(e)}", status_code=500)
