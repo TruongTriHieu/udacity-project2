f143ac1a9269488f2e2bd61426ad2d26cce23627
TruongHieu <TruongTriHieu@gmail.com>
2024-09-07 18:30:05 +0700

    update code

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,18 +1,18 @@
-#--------- Flask settings
-SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
-SERVER_PORT = 5000
-FLASK_DEBUG = True # Do not use debug mode in prod
+#--------- Cài đặt Flask
+SERVER_HOST = '0.0.0.0' # Lắng nghe trên tất cả các địa chỉ IP
+SERVER_PORT = 8000 # Cổng mà Azure yêu cầu
+FLASK_DEBUG = False # Tắt chế độ debug cho môi trường sản xuất
 
-# Flask-Restplus settings
+# Cài đặt Flask-Restplus
 SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_404_HELP = True
 API_VERSION = 'v1'
 
-#-------- Azure constants
+#-------- Các hằng số của Azure
 
-# API_URL format: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net/api"
-# Update this with your deployed Function App URL
+# API_URL định dạng: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net/api"
+# Cập nhật URL của Function App đã triển khai
 API_URL = "https://myuniqueapp2024.azurewebsites.net/api"
-#PI_URL = "http://localhost:7071/api"
+# API_URL = "http://localhost:7071/api" # Bỏ comment dòng này để kiểm tra trên máy local
