5a2103d928e89e0884b77fd568a711568922c2da
TruongHieu <TruongTriHieu@gmail.com>
2024-09-07 21:26:01 +0700

    update code

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,5 +1,5 @@
 #--------- Cài đặt Flask
-SERVER_HOST = 'https://neighborly-e2avdrgaesdcamdx.southeastasia-01.azurewebsites.net/' 
+SERVER_HOST = 'https://neighborly-e2avdrgaesdcamdx.southeastasia-01.azurewebsites.net' 
 SERVER_PORT = 5000 
 FLASK_DEBUG = True 
 
@@ -11,6 +11,6 @@ API_VERSION = 'v1'
 
 
 # API_URL định dạng: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net/api"
-# Cập nhật URL của Function App đã triển khai
+
 API_URL = "https://myuniqueapp2024.azurewebsites.net/api"
 # API_URL = "http://localhost:7071/api" # Bỏ comment dòng này để kiểm tra trên máy local
